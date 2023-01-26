a82d6079375d7577c5f7477dc1ff5d4abc1d0d40
Airbus5717 <Abdussamadf350@gmail.com>
2023-01-26 20:10:10 +0300

    add a few macros

--- code/macros.asm
+++ code/macros.asm
@@ -9,18 +9,31 @@
 .end_macro
 
 # print strings
-# param(s): string input
+# param(s): string address
 .macro print_str(%s)
 	la $a0, %s
 	li $v0, 4
 	syscall
 .end_macro
 
+# print string literals
+# param(s): string literal
+.macro print_strlit(%s)
+.data
+string_pointer: .asciiz %s
+.text
+	la $a0, string_pointer
+	li $v0, 4
+	syscall
+.end_macro
+
 # print int
-# param(d): int input
+# param(d): int register
 .macro print_int(%d)
 	la $a0, %d
 	li $v0, 1
 	syscall
 .end_macro
 
+# TODO:
+# - for loop (maybe?)
